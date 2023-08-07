9859d997b361cd4ee03682d91604018cd98b8bce
mandis-ncs <amnd.castro@gmail.com>
2023-08-07 15:21:32 -0300

    [1.1] uploading initial folder

---
+++ blog-pass/src/main/java/br/com/compass/pb/blogpass/BlogPassApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.blogpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPassApplication.class, args);
+	}
+
+}
---
+++ blog-pass/src/test/java/br/com/compass/pb/blogpass/BlogPassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.blogpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
