942ea6fc1c9dfbe491d97079e08d1ddc01e31d43
mandis-ncs <amnd.castro@gmail.com>
2023-08-13 16:27:07 -0300

    [6.5] refactoring filter config, have bcrypt problem

--- blog-pass/src/main/java/br/com/compass/pb/blogpass/config/SecurityConfig.java
+++ blog-pass/src/main/java/br/com/compass/pb/blogpass/config/SecurityConfig.java
@@ -2,30 +2,49 @@ package br.com.compass.pb.blogpass.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.config.http.MatcherType.mvc;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-        return http.build();
-    }
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+    CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+    requestHandler.setCsrfRequestAttributeName("_csrf");
+
+    http
+            .httpBasic().and()
+            .csrf(csrf -> csrf.disable())
+            .authorizeRequests(requests ->
+                    requests
+                            .requestMatchers(mvc.pattern("/h2-console/**")).permitAll()
+                            .requestMatchers(mvc.pattern("/blog-pass/auth/**")).permitAll()
+//                            .requestMatchers(mvc.pattern("/posts")).permitAll()
+//                            .requestMatchers(HttpMethod.GET, mvc.pattern("/posts/**").hasRole()
+                            .anyRequest().authenticated()
+            );
+    return http.build();
+}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
     }
 }
